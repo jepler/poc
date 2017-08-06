a489d18ef597eceef2e5c5e322574e4fde66d10d
Jeff Epler <jepler@unpythonic.net>
2017-08-05 23:08:33 -0500

    mock occmodule for doc build

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.modules['occmodel'] = object()
 
 # -- General configuration ------------------------------------------------
 
